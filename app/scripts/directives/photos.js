38d2eeaa62e0bcbcc62153d00f3359b48485e0e3
THISS <brenton.holswich@gmail.com>
2015-04-28 15:45:58 -0700

    filled out things for proposal and details. Extracted hero and photos into directives

---
+++ app/scripts/directives/photos.js
@@ -0,0 +1,33 @@
+angular.module('WeddingApp').directive('photos',function(){
+  return {
+    replace: true,
+    restrict: 'E',
+    require: "?ngModel",
+    templateUrl: 'views/photo.html',
+    link: function(scope,element,attrs,required){
+      var activeCategory = {};
+      scope.categories = Category.query();
+
+      scope.isActive = function(category)
+      {
+        return activeCategory && activeCategory.id === category.id;
+      };
+
+      scope.toggleCategory = function(category){
+        if(category === activeCategory)
+        {
+          activeCategory = {};
+        }else
+        {
+          activeCategory = category;
+        }
+
+        required.$setViewValue(activeCategory);
+      };
+
+      required.$render = function(){
+        activeCategory = required.$viewValue;
+      };
+    }
+  };
+});
