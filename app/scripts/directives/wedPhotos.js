843594f73ace376e36f72c9f9822dfb05bbcc8d1
THISS <brenton.holswich@gmail.com>
2015-04-30 04:04:52 -0700

    working through the wedPhotos directive

--- app/scripts/directives/wedPhotos.js
+++ app/scripts/directives/wedPhotos.js
@@ -1,32 +1,48 @@
 angular.module('WeddingApp').directive('wedPhotos',function(){
   // *** This is from the soup to bits codeschool staying sharp with angular
   return {
-  //   require: "?ngModel",
-  //   templateUrl: 'views/photo.html',
-  //   link: function(scope,element,attrs,required){
-  //     var activeCategory = {};
-  //     scope.categories = Category.query();
-  //
-  //     scope.isActive = function(category)
-  //     {
-  //       return activeCategory && activeCategory.id === category.id;
-  //     };
-  //
-  //     scope.toggleCategory = function(category){
-  //       if(category === activeCategory)
-  //       {
-  //         activeCategory = {};
-  //       }else
-  //       {
-  //         activeCategory = category;
-  //       }
-  //
-  //       required.$setViewValue(activeCategory);
-  //     };
-  //
-  //     required.$render = function(){
-  //       activeCategory = required.$viewValue;
-  //     };
-  //   }
+    require: "?ngModel",
+    templateUrl: 'views/photo.html',
+    controller: function($scope){
+      // We set the photoset object in the controller as this will be our point of reference
+      $scope.photoset = {};
+      // $scope.photoset.categories = [{name: catName, id: #}];
+      // $scope.photoset.photos = [{url: imageurl, categoryId: #}];
+    },
+    link: function(scope,element,attrs,ngModelCtrl){
+      var activeCategory = {};
+      scope.categories = [
+        {name: "Our STD's",id:1},
+        {name: "Couch Fun",id:2},
+        {name: "Lake Day",id:3},
+        {name: "Our Journey",id:4}
+      ];
+
+      scope.isActive = function(category)
+      {
+        return activeCategory.id === category.id;
+      };
+
+      scope.setCategory = function(category){
+        if(category === activeCategory)
+        {
+          activeCategory = {};
+        }else
+        {
+          activeCategory = category;
+        }
+
+        ngModelCtrl.$setViewValue(activeCategory);
+      };
+
+      scope.reset = function(){
+        activeCategory = {}
+        ngModelCtrl.$setViewValue(activeCategory);
+      };
+
+      ngModelCtrl.$render = function(){
+        activeCategory = ngModelCtrl.$viewValue;
+      };
+    }
   };
 });
